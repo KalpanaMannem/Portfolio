d231ae2a9bd1e5e99b84744937755c8e7e436228
KalpanaMannem <mannem.kalpana13@gmail.com>
2024-08-17 13:58:25 -0500

    Updated comments

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt, faPhone, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const Contact = () => (
   <section className="contact" id="contact">
